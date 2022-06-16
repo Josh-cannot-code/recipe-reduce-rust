8995b6a57968bce1f604b97f0b2f5cb9ad7caa79
Joshua Dall'Acqua <joshd@Joshuas-MacBook-Pro-2.local>
2022-06-15 20:13:28 -0600

    initial commit

---
+++ target/wasm32-unknown-unknown/debug/deps/recipe_reduce_rust-f4d97a65d84789d9.d
@@ -0,0 +1,5 @@
+/Users/joshd/recipe-reduce-rust/target/wasm32-unknown-unknown/debug/deps/recipe_reduce_rust-f4d97a65d84789d9.wasm: src/main.rs
+
+/Users/joshd/recipe-reduce-rust/target/wasm32-unknown-unknown/debug/deps/recipe_reduce_rust-f4d97a65d84789d9.d: src/main.rs
+
+src/main.rs:
---
+++ target/wasm32-unknown-unknown/debug/recipe-reduce-rust.d
@@ -0,0 +1,1 @@
+/Users/joshd/recipe-reduce-rust/target/wasm32-unknown-unknown/debug/recipe-reduce-rust.wasm: /Users/joshd/recipe-reduce-rust/src/main.rs
