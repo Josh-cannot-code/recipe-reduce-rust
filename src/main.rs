0c1cefe00018349a80f7208d2c54f754845d3577
jggeiger <107611621+jggeiger@users.noreply.github.com>
2022-06-21 18:19:08 -0600

    Added YEW component to change color of text (#12)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use yew::{html, Component, Context, Html};
+
+// Define the possible messages which can be sent to the component
+pub enum Msg {
+    ChangeToBlue,
+    ChangeToRed,
+}
+
+pub struct Model {
+    value: String, // This will store the current color
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: "color:blue".to_string() }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::ChangeToBlue => {
+                self.value = "color:blue".to_string();
+                true // Return true to cause the displayed change to update
+            }
+            Msg::ChangeToRed => {
+                self.value = "color:red".to_string();
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        
+        html! {
+            <div>
+                <div class="panel">
+                    // A button to send the change to blue message
+                    <button onclick={ctx.link().callback(|_| Msg::ChangeToBlue)}>
+                        { "Change to Blue" }
+                    </button>
+
+                    // A button to send the change to red message
+                    <button onclick={ctx.link().callback(|_| Msg::ChangeToRed)}>
+                        { "Change to Red" }
+                    </button> 
+                </div>
+
+                // Display text with current color
+                <p style = {self.value.clone()}>
+                    { "Some Text" }
+                </p>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
